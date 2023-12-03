c3da16c7f0e402a2cce32b69f127f201fb6af805
Niket <niket.chandil@braneenterprises.com>
2023-12-03 15:41:35 +0530

    Learned about major Datatypes in javaScipt

---
+++ 02_dataTypes_ECMA_Standards.js
@@ -0,0 +1,34 @@
+// DataTypes and ECMA Standards
+// Tutorial Followed :   Datatypes and ECMA standards | chai aur #javascript  
+// Link: https://www.youtube.com/watch?v=-9knnv97wSc&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=5
+// ChannelName : Chai aur Code
+
+
+"use strict"; //treat all JS code as newer JS version
+
+//By default latest version of JS uses "use Strict" Mode
+
+//alert("hello folks")   //will throw error as we are using node js, not a browser.
+
+//ECMA International Organisation sets the standards for Java Script coding.
+
+let name = "Niket" //string dataType
+let last_name = 'Chandil' //string dataType
+let age = 22 //number dataType
+let isUser = false // bool dataType
+
+//null => is dataType as well a standalone value , Representation of empty value
+//undefined => case when variable declared but not yet defined or initialised.
+//symbol => unique (will use further in React and Component based framework)
+//Object 
+
+//To know the dataType of any variable we can use "typeof()"
+
+console.log(typeof age ) //number
+console.log(typeof name ) //string
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
+
+
+//----Thank you ---- //  
+    
